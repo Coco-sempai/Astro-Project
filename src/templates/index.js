8d6f2f32c8a189b29badf69da28e9ef204c55f9b
Corentin <corentin.deoli70@hotmail.fr>
2024-12-12 22:32:11 +0100

    First commit

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,12 +1,16 @@
 import HomePage from './HomePage.astro';
 import TireIndexPage from './TireIndex.astro';
 import TireShowPage from './TireShow.astro';
+import RimsIndexPage from './RimsIndex.astro';
+import RimsShowPage from './RimsShow.astro';
 import NotFoundPage from './NotFoundPage.astro';
 
 const templateComponents = {
     '@apostrophecms/home-page': HomePage,
-    '@apostrophecms/tire-page:index': TireIndexPage,
-    'apostrophecms/tire-page:show': TireShowPage,
+    'tire-page:index': TireIndexPage,
+    'tire-page:show': TireShowPage,
+    'rims-page:index': RimsIndexPage,
+    'rims-page:show': RimsShowPage,
     '@apostrophecms/page:notFound': NotFoundPage
 };
 
